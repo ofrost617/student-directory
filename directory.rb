af8eb102c7d851302f32d220adf642df52efdc11
Olivia Frost <olivia.frost617@googlemail.com>
2017-07-19 10:06:42 +0100

    Cohort name as symbol and data storage in hash

--- directory.rb
+++ directory.rb
@@ -1,16 +1,15 @@
 #First create a student array
 students = [
-"Dr. Hannibal Lecter",
-"Darth Vader",
-"Nurse Ratched",
-"Michael Corleone",
-"Alex DeLarge",
-"The Wicked Witch of the West",
-"Terminator",
-"Freddy Krueger",
-"The Joker",
-"Joffrey Baratheon",        
-"Norman Bates"
+  {name: "Dr. Hannibal Lecter", cohort: :november},
+  {name: "Darth Vader", cohort: :november},
+  {name: "Nurse Ratched", cohort: :november},
+  {name: "Michael Corleone", cohort: :november},
+  {name: "Alex DeLarge", cohort: :november},
+  {name: "The Wicked Witch of the west", cohort: :november},
+  {name: "Terminator", cohort: :november},
+  {name: "Freddy Kreuger", cohort: :november},
+  {name: "The Joker", cohort: :november},
+  {name: "Norman Bates", cohort: :november}
 ]
 
 #And then print them
@@ -19,8 +18,8 @@ def print_header
     puts "------------"
 end
 
-def print(names)
-    names.each { |name| puts name }
+def print(students)
+    students.each { |student| puts "#{student[:name]} (#{student[:cohort]} cohort)" }
 end
 
 def print_footer(names)
