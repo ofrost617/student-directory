2b26e0e616a054e72eff7a96fd534877c4968c24
Olivia Frost <olivia.frost617@googlemail.com>
2017-07-20 12:01:06 +0100

    Step 9 complete

--- directory.rb
+++ directory.rb
@@ -18,6 +18,7 @@ def input_students
     students
 end
 
+
 #And then print them
 def print_header
     puts "The students of Villains Academy"
@@ -32,11 +33,52 @@ def print_footer(names)
     puts "Overall, we have #{names.count} great students"
 end
 
+def interactive_menu
+  students = [] # declared as an empty array before the loop so that it is available to several iterations of the loop
+  loop do
+    # 1. Print the menu and ask the user what to do
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit"
+    # 2. Read the input and save it to a variable
+    selection = gets.chomp
+    #3. do what the user has asked
+    case selection
+      when  "1"
+        students = input_students
+      when "2"
+        print_header
+        print(students)
+        print_footer(students)
+      when "9"
+        exit # this wil cause the program to terminate
+      else 
+        puts "I don't know what you meant, try again"
+      end
+  end
+end
+  
+
+
 #nothing will happpen until we call the methods
+interactive_menu
 students = input_students
 print_header
 print(students)
 print_footer(students)
 
 
+# Step 8.2 
+# only print students names starting with certain letter:
+# either: change while statement to !name.empty? && name.start_with?("d") ... 
+        # BUT this would stop inputs as soon as a name doesn't begin with "d"
+# or: use array.select
 
+#students.each do |student| 
+  #  puts student 
+   # student.first == "d"
+#end 
+
+  #  def select_students_starting_with(students, letter)
+   #   students.select { |str| str.start_with?("d") }
+    #end
