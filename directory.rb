888bcf5cd695a71e4f13489a6cdbc34f386efe99
Olivia Frost <olivia.frost617@googlemail.com>
2017-07-22 13:30:07 +0100

    Step 11 complete

--- directory.rb
+++ directory.rb
@@ -17,6 +17,8 @@ def process(selection)
         input_students
       when "2"
         show_students
+      when "3"
+        save_students
       when "9"
         exit
       else
@@ -30,6 +32,7 @@ def process(selection)
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list to students.csv"
   puts "9. Exit"
 end
   # Define a show_students method
@@ -74,6 +77,19 @@ def print_footer(names)
   puts "Overall, we have #{names.count} great students"
 end
 
+def save_students
+  # Open the file for writing
+  file = File.open("students.csv", "w") #Here we have opened a file using the .open method and saved the file as a variable called 'file'
+  # Iterate over the array of students
+  @students.each do |student| #Here we iterate over the array of students, processing one at a time into the file
+    student_data = [student[:name], student[:cohort]] #on every iteration we take the data in the hash and create a new array with it
+    # to show the student name and cohort
+    csv_line = student_data.join(",") # we then convert the new array into a comma separated string here, .join turns the elements of the array
+    # it's called on (in this case student_data), using it's argument as a separator
+    file.puts csv_line # calling puts on a file writes to a file not the screen, in this case it's writing csv_line to a file
+  end
+  file.close
+end 
 
 # Call the methods:
 interactive_menu
