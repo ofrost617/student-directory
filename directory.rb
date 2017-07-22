79a4a611bfc5382ad9cdff06f5ce088a4e8df4e1
Olivia Frost <olivia.frost617@googlemail.com>
2017-07-22 14:32:47 +0100

    Step 12 complete

--- directory.rb
+++ directory.rb
@@ -7,7 +7,7 @@ def interactive_menu
 # 4. Repeat from step 1
   loop do 
     print_menu
-    process(gets.chomp)
+    process(STDIN.gets.chomp)
   end
 end
 
@@ -52,14 +52,14 @@ def input_students
   puts "To finish, just hit return twice" 
  #Create empty array for students to go into
 # Get the first name
-  name = gets.chomp
+  name = STDIN.gets.chomp
 # While the name is not empty, repeat this code. Empty method is part of the string class
   while !name.empty? do
 # Add the student hash to the array. << is the shovel operator and is used to add things into an array
     @students << {name: name, cohort: :november}
     puts "Now we have #{@students.count} students"
 # get another name from the user
-    name = gets.chomp
+    name = STDIN.gets.chomp
   end
 # Return the array of students
   @students
@@ -94,8 +94,8 @@ def save_students
   file.close
 end 
 
-def load_students
-  file = File.open("students.csv", "r")
+def load_students(filename = "students.csv")
+  file = File.open(filename, "r")
   file.readlines.each do |line|
     name, cohort = line.chomp.split(',')
     @students << {name: name, cohort: cohort.to_sym}
@@ -103,6 +103,21 @@ def load_students
   file.close
 end
 
+def try_load_students
+  filename = ARGV.first
+  return if filename.nil?
+  if File.exists?(filename)
+    load_students(filename)
+    puts "Loaded #{@students.count} from #{filename}"
+  else
+    puts "Sorry, #{filename} doesn't exist"
+    exit
+  end
+  
+end
+
+
 # Call the methods:
+load_students
 interactive_menu
 
